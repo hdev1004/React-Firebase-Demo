0f04ab350259313808d6860b1212e413640ba3e8
Jin <H.dev@kakao.com>
2021-07-07 17:29:15 +0900

    겁나 힘드네 이거

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import firebaseInit from './firebaseInit';
 
 function App() {
+  // firebaseInit에서 넘어온 정보를 콘솔에 출력
+  console.log(firebaseInit)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1-ML4JoGkeC0Fe4QnWnGwbV3g3auP0RY",
+  authDomain: "usermanage-0210.firebaseapp.com",
+  databaseURL: "https://usermanage-0210-default-rtdb.firebaseio.com",
+  projectId: "usermanage-0210",
+  storageBucket: "usermanage-0210.appspot.com",
+  messagingSenderId: "58016895101",
+  appId: "1:58016895101:web:6e03de76d08a24cde12114",
+  measurementId: "G-12T8XPW5E3"
+};
+
+export default firebase.initializeApp(firebaseConfig);
