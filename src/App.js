88db4b84f457a196dbbecb7a97f1561888f3bd12
Jin <H.dev@kakao.com>
2021-07-07 22:30:07 +0900

    어렵다

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import firebaseInit from './firebaseInit';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 function App() {
+  console.log(firebaseInit.database);
   // firebaseInit에서 넘어온 정보를 콘솔에 출력
-  console.log(firebaseInit)
   return (
     <div className="App">
       <header className="App-header">
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,8 +1,9 @@
-import firebase from "firebase/app";
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyA1-ML4JoGkeC0Fe4QnWnGwbV3g3auP0RY",
   authDomain: "usermanage-0210.firebaseapp.com",
@@ -12,6 +13,12 @@ const firebaseConfig = {
   messagingSenderId: "58016895101",
   appId: "1:58016895101:web:6e03de76d08a24cde12114",
   measurementId: "G-12T8XPW5E3"
-};
+}; 
 
-export default firebase.initializeApp(firebaseConfig);
+class Fire { 
+  constructor() {
+    this.init = firebase.initializeApp(firebaseConfig);
+    this.database = firebase.database();
+  }
+}
+export default new Fire();
